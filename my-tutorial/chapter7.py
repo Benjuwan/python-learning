f95bba8fc22a5c3f4fd9e1a2c45db953fb038b82
benjuwan_5931 <benjuwan.kokusaig@gmail.com>
2025-03-03 16:39:06 +0900

    fix: チュートリアル進行（チャプター8: 例外処理.）

--- my-tutorial/chapter7.py
+++ my-tutorial/chapter7.py
@@ -24,6 +24,7 @@ class Pass:
 pass_class = Pass()
 
 
+# 基底クラス
 class Item:
     def __init__(self, name, price):
         self.name = name
@@ -33,6 +34,7 @@ class Item:
         print(f"{self.name}: {self.price}")
 
 
+# 派生クラス（単一継承）
 class Food(Item):
     # 基底クラスの __init__ 引数も指定する（今回は name, price）
     def __init__(self, name, price, used_limit):
@@ -45,7 +47,11 @@ class Food(Item):
         super().show()
         print(f"{self.name}: {self.price}: {self.used_limit}")
 
+    def eat(self):
+        print(f"--- eating {self.name}.")
 
+
+# 派生クラス（単一継承）
 class Toy(Item):
     def __init__(self, name, price, target_age):
         super().__init__(name, price)
@@ -55,8 +61,39 @@ class Toy(Item):
         # super().show()
         print(f"{self.name}: {self.price}: {self.target_age}")
 
+    def play(self):
+        print(f"--- playing with {self.name}.")
+
 
 Food_instance = Food("chocolate", 100, 180)
 Food_instance.show()
+Food_instance.eat()
 Toy_instance = Toy("figure", 350, 3)
 Toy_instance.show()
+Toy_instance.play()
+
+
+# 派生クラス（多重継承） ちなみに、Food, Toy の記述順を逆にすると？
+class Syokugan(Food, Toy):
+    def __init__(self, name, price, used_limit, target_age):
+        # 以下の方法（super）だと{ Food.__init__() takes 4 positional arguments but 5 were given }というエラーが発生する
+        # super().__init__(name, price, used_limit, target_age)
+        self.name = name
+        self.price = price
+        self.used_limit = used_limit
+        self.target_age = target_age
+
+    def eat(self):
+        print(f"--- eating {self.name.split('_')[0]}.")
+
+    def play(self):
+        print(f"--- playing with {self.name.split('_')[1]}.")
+
+    def show(self):
+        super().show()
+
+
+Syokugan_instance = Syokugan("Choco_Doll", 450, 180, 3)
+Syokugan_instance.show()
+Syokugan_instance.eat()
+Syokugan_instance.play()
