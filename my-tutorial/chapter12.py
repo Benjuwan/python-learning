da08e876cdcbb820fcec4cc2d863c4e2f1983a7d
benjuwan_5931 <benjuwan.kokusaig@gmail.com>
2025-05-15 16:31:33 +0900

    fix: 既存ファイルの修正（不要な記述の削除）.

--- my-tutorial/chapter12.py
+++ my-tutorial/chapter12.py
@@ -3,29 +3,6 @@
 import email.mime.text as theMailer  # メール作成のライブラリ
 import smtplib  # メールを送信するためのライブラリ
 
-# 区切り（兼改行）用の文字列
-duplicate_counter: int = 0
-duplicate_str: str = "-+-"
-
-
-# 区切り（兼改行）用の文字列を表示する関数
-def duplicate_count(duplicate_counter: int) -> None:
-    """[duplicate_count 関数の概要]
-    Args:
-        duplicate_counter: int # 区切り見出し番号
-
-    Returns:
-        None # JavaScript でいう void
-    """
-    if duplicate_counter == 1:
-        print(f"\n{str(duplicate_counter)} {duplicate_str * 20} \n")
-    else:
-        print(f"{str(duplicate_counter)} {duplicate_str * 20} \n")
-
-
-duplicate_counter += 1
-duplicate_count(duplicate_counter)
-
 # 複数行の指定・記述は三重クオートが便利
 dammy_txt = """
 Lorem ipsum dolor sit amet,
